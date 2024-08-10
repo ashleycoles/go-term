14842c540737c97b9c52ceb1aac799cc14032ec2
Ash Coles <ashley@acoles.co.uk>
2024-08-10 21:15:39 +0100

    append traversal

--- commands/fappend.go
+++ commands/fappend.go
@@ -7,10 +7,34 @@ import (
 )
 
 func fappend(command Command, activeDirectory *filesystem.Directory) {
+	if command.ArgsCount() != 2 {
+		terminal.NewLine()
+		fmt.Print("append: must specify a filepath and content")
+		terminal.NewLine()
+		return
+	}
+
 	name := command.Args[0]
 	content := command.Args[1]
 
-	file, err := (*activeDirectory).GetFile(name)
+	path := filesystem.ParsePath(name)
+
+	if !path.HasFile() {
+		terminal.NewLine()
+		fmt.Printf("append: %s is not a file", name)
+		terminal.NewLine()
+	}
+
+	targetDirectory, err := (*activeDirectory).Traverse(name)
+
+	if err != nil {
+		terminal.NewLine()
+		fmt.Printf("append: %s", err.Error())
+		terminal.NewLine()
+		return
+	}
+
+	file, err := targetDirectory.GetFile(*path.File)
 
 	if err != nil {
 		terminal.NewLine()
